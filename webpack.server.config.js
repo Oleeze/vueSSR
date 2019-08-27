f2fb7d28f63b5a8f9578cf7398e8d100d66f652c
Oleg Rudenko <orudenkony@gmail.com>
2019-08-26 21:22:59 -0600

    Vue app is rendering from server

---
+++ webpack.server.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const webpack = require('webpack')
+const VueSSRPlugin = require('vue-ssr-webpack-plugin')
+
+module.exports = {
+  target: 'node',
+  entry: './src/main.server.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js',
+    libraryTarget: 'commonjs'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  performance: {
+    hints: false
+  },
+  externals: Object.keys(require('./package.json').dependencies),
+  devtool: 'source-map',
+
+  plugins: [
+    new VueSSRPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
+}
