bc38d15054c50fd6f4ec71542ed4d84a494dd0c9
Oleg Rudenko <orudenkony@gmail.com>
2019-08-27 21:01:39 -0600

    Fetching data from cocktail api

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-#!/usr/bin/env node
-
 const fs = require('fs');
 const express = require('express');
+const axios = require('axios');
+
 const { createBundleRenderer } = require('vue-server-renderer');
 
 const bundleRenderer = createBundleRenderer(
-  // Load the SSR bundle with require.
   require('./dist/vue-ssr-bundle.json'),
   {
-    // Yes, I know, readFileSync is bad practice. It's just shorter to read here.
     template: fs.readFileSync('./index.html', 'utf-8')
   }
 );
 
-// Create the express app.
 const app = express();
+const port = process.env.PORT || 8080;
 
-// Serve static assets from ./dist on the /dist route.
 app.use('/dist', express.static('dist'));
 
-// Render all other routes with the bundleRenderer.
+app.get('/test', (req, res) => {
+  axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
+  .then(response => {
+
+    console.log('Hello: ' ,res.data)
+    res.send(response.data);
+  })
+  .catch(err => res.send(err))
+})
+
 app.get('*', (req, res) => {
   bundleRenderer
-    // Renders directly to the response stream.
-    // The argument is passed as "context" to main.server.js in the SSR bundle.
     .renderToStream({url: req.path})
     .pipe(res);
 });
 
-// Bind the app to this port.
-app.listen(8080);
+app.listen(port, () => console.log(`Server running on port : ${port}`));
