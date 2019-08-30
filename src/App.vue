9140fb6ac3ec64420878b23d925f896f0a3890ed
Oleg Rudenko <orudenkony@gmail.com>
2019-08-29 19:46:04 -0600

    Data parsing and styling random drink cards

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    Hello World!
-    <p>
+    <!-- <p>
       <router-link to="/">Go To Home</router-link>
       <router-link to="/about">Go To About</router-link>
-    </p>
+    </p>-->
     <router-view></router-view>
   </div>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
-    <form>
-      <div v-for="(liquor, index) of liquors" :key="index">
-        <input v-model="checkedLiquor" type="checkbox" :id="liquor" :value="liquor">
-        <label :for="liquor">{{liquor}}</label>
-      </div>
-    </form>
-    <button v-on:click="test">Get Drinks</button>
-    <h1>Look at all these drinks</h1>
-    <h3 v-for="(drink, index) of listOfDrinks" :key="index">Drink {{drink}}</h3>
-        <button v-on:click="getRandom">Get Random Drink</button>
+    <div class="randomButtonWrapper">
+      <button v-on:click="getRandom">Get Random Drink</button>
+    </div>
 
-    <div v-for="(randomDrink, index) of arrayRandom" :key="index">
+    <div class="randomDrinksWrapper">
+      <div class="randomDrinkItem" v-for="(randomDrink, index) of arrayRandom" :key="index">
         <p>Name: {{randomDrink.strDrink }}</p>
         <p>Type: {{randomDrink.strAlcoholic}}</p>
-        {{randomDrink.strDrinkThumb}}
-
-        <img data-original={{index}} v-bind:src="randomDrink.strDrinkThumb">
-
+        <p>Glass: {{randomDrink.strGlass}}</p>
+        <p>Instructions: {{randomDrink.strInstructions}}</p>
+        <p>Ingredients:</p>
+        <p
+          v-for="(value, name, index) in randomDrink.strIngredients"
+          :key="index"
+        >{{name}} : {{value}}</p>
+        <img data-original="{{index}}" v-bind:src="randomDrink.strDrinkThumb">
+      </div>
     </div>
   </div>
 </template>
@@ -51,25 +50,65 @@ export default {
     },
     getRandom() {
       let obj = {
-        strDrink : '',
-        strAlcoholic: '',
-        strGlass: '',
-        strInstructions: '',
-        strDrinkThumb: '',
+        strDrink: "",
+        strAlcoholic: "",
+        strGlass: "",
+        strInstructions: "",
+        strDrinkThumb: "",
         strIngredients: {}
-      }
-      axios.get('/random').then(res => {
-        console.log("This is the response: ",res.data[0])
+      };
+      axios.get("/random").then(res => {
         for (let value in res.data[0]) {
-          if (obj.hasOwnProperty(value)) {
-            obj[value] = res.data[0][value];
+          if (res.data[0][value] && res.data[0][value].length > 2) {
+            if (obj.hasOwnProperty(value)) {
+              obj[value] = res.data[0][value];
+            } else if (value.includes("strIngredient")) {
+              obj.strIngredients[res.data[0][value]] =
+                res.data[0]["strMeasure" + value.slice(-1)];
+            }
           }
         }
-          console.log("This is obj: ",obj)
-          this.arrayRandom.push(obj);
-          console.log(this.arrayRandom)
-      })
+        this.arrayRandom.unshift(obj);
+      });
     }
   }
 };
 </script>
+
+<style>
+body {
+  background: lightgray;
+  margin: 0;
+}
+
+.randomButtonWrapper {
+  background: white;
+  padding: 2em;
+}
+
+.randomButtonWrapper button {
+  display: block;
+  margin: auto;
+  padding: 1em;
+}
+
+.randomDrinksWrapper {
+  padding: 2em;
+}
+
+.randomDrinkItem {
+  border: 1px solid black;
+  border-radius: 12px;
+  background: white;
+  padding: 1em;
+  margin-top: 2em;
+}
+</style>
+<!-- <form>
+      <div v-for="(liquor, index) of liquors" :key="index">
+        <input v-model="checkedLiquor" type="checkbox" :id="liquor" :value="liquor">
+        <label :for="liquor">{{liquor}}</label>
+      </div>
+    </form>
+    <button v-on:click="test">Get Drinks</button>
+    <h3 v-for="(drink, index) of listOfDrinks" :key="index">Drink {{drink}}</h3>
