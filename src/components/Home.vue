26f40f3b461fbfa143f074b1093e97fef4dc6f22
Oleg Rudenko <orudenkony@gmail.com>
2019-09-02 15:11:42 -0600

    Working on styles and render lag

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,62 +3,42 @@
     <div class="randomButtonWrapper">
       <button v-on:click="getRandom">Get Random Drink</button>
     </div>
-
-    <div class="rDWrapper">
-      <div class="rDItem" v-for="(randomDrink, index) of arrayRandom" :key="index">
-        <div class="rDPrimary">
-          <div class="rDPS">
-            <img data-original="{{index}}" v-bind:src="randomDrink.strDrinkThumb">
-          </div>
-          <div class="rDPS">
-            <p>Name: {{randomDrink.strDrink }}</p>
-            <p>Type: {{randomDrink.strAlcoholic}}</p>
-            <p>Glass: {{randomDrink.strGlass}}</p>
-            <p>
-              <u>Ingredients</u>
-            </p>
-            <p
-              v-for="(value, name, index) in randomDrink.strIngredients"
-              :key="index"
-            >{{name}} : {{value}}</p>
-          </div>
-        </div>
-
-        <p class="underLine">Instructions</p>
-
-        <p>{{randomDrink.strInstructions}}</p>
-      </div>
-    </div>
+    <randomDrinkList :arrayRandom="arrayRandom"></randomDrinkList>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import RandomDrinkList from "../comps/Drink.vue";
 
 export default {
+  components: {
+    randomDrinkList: RandomDrinkList
+  },
+
   data() {
     return {
       liquors: ["vodka", "gin", "rum", "tequila", "whiskey"],
-      checkedLiquor: [],
-      listOfDrinks: [],
+      // checkedLiquor: [],
+      // listOfDrinks: [],
       arrayRandom: []
     };
   },
   methods: {
-    test() {
-      for (let liquor of this.liquors) {
-        axios
-          .get("test", { params: { liquor: liquor } })
-          .then(res => {
-            console.log(res.data);
-            this.listOfDrinks.push(res.data);
-          })
-          .catch(err => console.log(err));
-      }
+    // test() {
+    //   for (let liquor of this.liquors) {
+    //     axios
+    //       .get("test", { params: { liquor: liquor } })
+    //       .then(res => {
+    //         console.log(res.data);
+    //         this.listOfDrinks.push(res.data);
+    //       })
+    //       .catch(err => console.log(err));
+    //   }
 
-      // this.listOfDrinks.flat(1);
-      // console.log("This is the list of drinks" + this.listOfDrinks);
-    },
+    // this.listOfDrinks.flat(1);
+    // console.log("This is the list of drinks" + this.listOfDrinks);
+    // },
     getRandom() {
       let obj = {
         strDrink: "",
@@ -95,6 +75,9 @@ body {
 .randomButtonWrapper {
   background: #23395b;
   padding: 2em;
+  display: block;
+  z-index: 5;
+  position: relative;
 }
 
 .randomButtonWrapper button {
@@ -107,36 +90,6 @@ body {
   border-radius: 24px;
   border: 1px solid white;
   cursor: pointer;
-}
-
-.rDWrapper {
-  padding: 2em;
-}
-
-.rDItem {
-  border: 1px solid black;
-  border-radius: 12px;
-  background: white;
-  padding: 4em;
-  margin-top: 2em;
-}
-
-.rDItem img {
-  width: 100%;
-}
-
-.rDPrimary {
-  display: flex;
-  align-items: center;
-  text-align: center;
-}
-
-.rDPS {
-  flex: 1;
-}
-
-.underLine {
-  border-bottom: 1px solid black;
 }
 </style>
 <!-- <form>
---
+++ src/comps/Drink.vue
@@ -0,0 +1,122 @@
+<template v-cloak>
+  <div>
+    <div class="rDWrapper">
+      <div class="rDItem active" v-for="(randomDrink, index) of arrayRandom" :key="index">
+        <div class="rDPrimary" v-cloak>
+          <div class="rDPS">
+            <img v-on:load="hello(this)" :src="randomDrink.strDrinkThumb">
+          </div>
+          <div class="rDPS">
+            <p>Name: {{randomDrink.strDrink }}</p>
+            <p>Type: {{randomDrink.strAlcoholic}}</p>
+            <p>Glass: {{randomDrink.strGlass}}</p>
+            <p>
+              <u>Ingredients</u>
+            </p>
+            <p
+              v-for="(value, name, index) in randomDrink.strIngredients"
+              :key="index"
+            >{{name}} : {{value}}</p>
+          </div>
+        </div>
+
+        <p class="underLine">Instructions</p>
+
+        <p>{{randomDrink.strInstructions}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let self = this;
+
+export default {
+  props: ["arrayRandom"],
+  data() {
+    return {};
+  },
+  methods: {
+    loading: function() {
+      console.log("Enitre element has loaded");
+    },
+    hello: function(key) {
+      // console.log(key);
+      // console.log("This is the array: ", this.arrayRandom[0]);
+    }
+  }
+};
+</script>
+
+<style>
+[v-cloak] {
+  background: pink;
+}
+
+.rDWrapper {
+  padding: 2em;
+}
+
+.rDItem {
+  margin: auto;
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 12px;
+  background: white;
+  margin-top: 2em;
+  z-index: 1;
+  animation: keepslidein 0.4s;
+}
+
+.rDItem:first-of-type {
+  animation: slidein 0.4s;
+}
+
+.rDItem img {
+  width: 100%;
+}
+@keyframes slidein {
+  from {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes keepslidein {
+  from {
+    opacity: 0;
+    transform: translateY(0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(100);
+  }
+}
+
+.active {
+  background: palegreen;
+}
+
+.rDPrimary {
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+
+.rDPS {
+  flex: 1;
+}
+
+.underLine {
+  border-bottom: 1px solid black;
+}
+[v-cloak] {
+  background: pink;
+}
+</style>
